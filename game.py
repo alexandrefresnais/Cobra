8b13b1464a95f1919bbe79588e66eee8c1a77852
Alexandre <alexandre.fresnais@epita.fr>
2021-01-21 20:45:19 +0100

    [SPLIT] game.py into Apple.py

--- game.py
+++ game.py
@@ -5,25 +5,7 @@ import math
 
 from globals import *
 from Snake import Snake
-
-class Apple:
-    def __init__(self):
-        self.position = (0,0)
-        self.color = (223,163,49)
-        self.randomize_position()
-
-    def randomize_position(self, snake = None):
-        self.position = (random.randint(0, GRID_WIDTH - 1), random.randint(0, GRID_HEIGHT - 1))
-        # while position is taken by a part of snake
-        if snake != None:
-            while self.position in snake.positions:
-                self.position = (random.randint(0, GRID_WIDTH - 1), random.randint(0, GRID_HEIGHT - 1))
-
-    def draw(self, surface):
-        r = pygame.Rect((self.position[0] * GRIDSIZE, self.position[1] * GRIDSIZE), (GRIDSIZE, GRIDSIZE))
-        pygame.draw.rect(surface, self.color, r)
-        pygame.draw.rect(surface, (93,216,228), r, 1)
-
+from Apple import Apple
 
 def drawGrid(surface):
     for y in range(0, int(GRID_HEIGHT)):
