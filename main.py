e60f89ab87525d76daa190624f5fc2a5af446d67
Alexandre <alexandre.fresnais@epita.fr>
2021-01-21 20:54:48 +0100

    [ADD] Env class

--- main.py
+++ main.py
@@ -4,57 +4,15 @@ import random
 import math
 
 from globals import *
-from Snake import Snake
-from Apple import Apple
 
-def drawGrid(surface):
-    for y in range(0, int(GRID_HEIGHT)):
-        for x in range(0, int(GRID_WIDTH)):
-            if (x+y) % 2 == 0:
-                r = pygame.Rect((x*GRIDSIZE, y*GRIDSIZE), (GRIDSIZE, GRIDSIZE))
-                pygame.draw.rect(surface, (93, 216, 228), r)
-            else:
-                rr = pygame.Rect((x*GRIDSIZE, y*GRIDSIZE), (GRIDSIZE, GRIDSIZE))
-                pygame.draw.rect(surface, (84, 194, 205), rr)
+from Env import Env
 
 def main():
     pygame.init()
 
-    clock = pygame.time.Clock()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
-
-    surface = pygame.Surface(screen.get_size())
-    surface = surface.convert()
-    drawGrid(surface)
-
-    snake = Snake()
-    apple = Apple()
-
-    myfont = pygame.font.SysFont("monospace",16)
+    env = Env()
 
     while (True):
-        clock.tick(8)
-
-        snake.handle_keys()
-        drawGrid(surface)
-        snake.move()
-        if snake.get_head_position() == apple.position:
-            snake.length += 1
-            snake.score += 1
-            apple.randomize_position(snake)
-            if snake.length == GRID_WIDTH * GRIDSIZE:
-                # WON
-                snake.reset()
-
-        state = snake.get_state(apple)
-
-        snake.draw(surface)
-        apple.draw(surface)
-
-
-        screen.blit(surface, (0,0))
-        text = myfont.render("Score {0}".format(snake.score), 1, (0,0,0))
-        screen.blit(text, (5,10))
-        pygame.display.update()
+        env.step()
 
 main()
