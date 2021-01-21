eb799f10d51a6b037dd78d157564fe1468cef7c4
Alexandre <alexandre.fresnais@epita.fr>
2021-01-21 19:17:22 +0100

    [ADD] state vector calcul for machine learning

--- game.py
+++ game.py
@@ -2,9 +2,50 @@ import pygame
 import sys
 import random
 
+def same_sign(a, b):
+    return (a > 0) == (b > 0)
+
+# Nb grid square
+GRID_WIDTH = 12
+GRID_HEIGHT = 12
+
+# Absolute size of a grid square
+GRIDSIZE = 20
+
+# Size of window
+SCREEN_WIDTH = GRID_WIDTH * GRIDSIZE
+SCREEN_HEIGHT = GRID_HEIGHT * GRIDSIZE
+
+UP = (0, -1)
+DOWN = (0, 1)
+LEFT = (-1, 0)
+RIGHT = (1, 0)
+
+# important : clockwise
+directions = [UP, RIGHT, DOWN, LEFT]
+
+# get local right from our current direction
+def get_local_right(direction):
+    i = directions.index(direction) + 1
+    return directions[i % 4]
+
+def get_local_down(direction):
+    i = directions.index(direction) + 2
+    return directions[i % 4]
+
+def get_local_left(direction):
+    i = directions.index(direction) + 3
+    return directions[i % 4]
+
+def add_tuple(a, b):
+    res = (0, 0)
+    res[0] = a[0] + b[0]
+    res[1] = a[1] + b[1]
+    return res
+
 class Cobra:
     def __init__(self):
-        self.length = 1
+        self.length = 8
         self.positions = [((GRID_WIDTH / 2), GRID_HEIGHT / 2)]
         self.direction = random.choice([UP, DOWN, LEFT, RIGHT])
         self.color = (17,24,47)
@@ -69,6 +110,55 @@ class Cobra:
                 elif event.key == pygame.K_RIGHT:
                     self.turn(RIGHT)
 
+    def get_state(self, apple):
+        # [0:3] : apple location relatively to our direction
+        # [4:7] obstacle presence
+        # [8:11] snake direction
+
+        state = [0] * 12
+
+        # if apple not strictly on the right or left of snake
+        if not ((self.direction[0] == 0 or self.positions[0][0] == apple.position[0]) and (self.direction[1] == 0 or self.positions[0][1] == apple.position[1])):
+            # If above (= in the direction)
+            state[0] = (self.direction[0] == 0 or same_sign(self.direction[0], apple.position[0] - self.positions[0][0])) and (self.direction[1] == 0 or same_sign(self.direction[1], apple.position[1] - self.positions[0][1]))
+            # it is behind if not above and if not on same line
+            state[2] = not state[0]
+
+        r_dir = get_local_right(self.direction)
+        # if apple not strictly on above or behind snake
+        if not ((r_dir[0] == 0 or self.positions[0][0] == apple.position[0]) and (r_dir[1] == 0 or self.positions[0][1] == apple.position[1])):
+            # if on the right of the snake
+            state[1] = (r_dir[0] == 0 or same_sign(r_dir[0], apple.position[0] - self.positions[0][0])) and (r_dir[1] == 0 or same_sign(r_dir[1], apple.position[1] - self.positions[0][1]))
+            # it is on left if not on right and if not on same line
+            state[3] = not state[1]
+
+        # going forward to see if obstable
+        tmp = tuple(map(sum, zip(self.positions[0], self.direction))) # Python, i just want to add two tuple please
+        if tmp[0] < 0 or tmp[1] < 0 or tmp[0] >= GRID_WIDTH or tmp[1] >= GRID_HEIGHT or tmp in self.positions:
+            state[4] = 1
+
+        tmp = tuple(map(sum, zip(self.positions[0], get_local_right(self.direction))))
+        if tmp[0] < 0 or tmp[1] < 0 or tmp[0] >= GRID_WIDTH or tmp[1] >= GRID_HEIGHT or tmp in self.positions:
+            state[5] = 1
+
+        tmp = tuple(map(sum, zip(self.positions[0], get_local_down(self.direction))))
+        if tmp[0] < 0 or tmp[1] < 0 or tmp[0] >= GRID_WIDTH or tmp[1] >= GRID_HEIGHT or tmp in self.positions:
+            state[6] = 1
+
+        tmp = tuple(map(sum, zip(self.positions[0], get_local_left(self.direction))))
+        if tmp[0] < 0 or tmp[1] < 0 or tmp[0] >= GRID_WIDTH or tmp[1] >= GRID_HEIGHT or tmp in self.positions:
+            state[7] = 1
+
+        # Setting direction
+        state[8] = self.direction == UP
+        state[9] = self.direction == RIGHT
+        state[10] = self.direction == DOWN
+        state[11] = self.direction == LEFT
+
+        return state
+
+
+
 class Apple:
     def __init__(self):
         self.position = (0,0)
@@ -98,22 +188,6 @@ def drawGrid(surface):
                 rr = pygame.Rect((x*GRIDSIZE, y*GRIDSIZE), (GRIDSIZE, GRIDSIZE))
                 pygame.draw.rect(surface, (84, 194, 205), rr)
 
-# Nb grid square
-GRID_WIDTH = 12
-GRID_HEIGHT = 12
-
-# Absolute size of a grid square
-GRIDSIZE = 20
-
-# Size of window
-SCREEN_WIDTH = GRID_WIDTH * GRIDSIZE
-SCREEN_HEIGHT = GRID_HEIGHT * GRIDSIZE
-
-UP = (0, -1)
-DOWN = (0, 1)
-LEFT = (-1, 0)
-RIGHT = (1, 0)
-
 def main():
     pygame.init()
 
@@ -130,7 +204,7 @@ def main():
     myfont = pygame.font.SysFont("monospace",16)
 
     while (True):
-        clock.tick(8)
+        clock.tick(2)
 
         snake.handle_keys()
         drawGrid(surface)
@@ -142,6 +216,8 @@ def main():
             if snake.length == GRID_WIDTH * GRIDSIZE:
                 # WON
                 snake.reset()
+
+        state = snake.get_state(food)
 
         snake.draw(surface)
         food.draw(surface)
